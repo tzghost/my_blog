183ef4ecb075d05d2a97adfff5ea6bfdf2e55cfe
taojianwen <taojianwen@wtsd.cn>
2020-07-23 14:12:43 +0800

    1、配置host

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@+z-92gxrhb2wg*7)ij--=9$8ep2^r+9-zwcqlk)#fpw+3an(h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "www.bruketao.cn"]
 
 
 # Application definition
@@ -144,15 +144,15 @@ STATIC_URL = '/static/'
 # SMTP服务器，改为你的邮箱的smtp!
 EMAIL_HOST = 'smtp.qq.com'
 # 改为你自己的邮箱名！
-EMAIL_HOST_USER = '310533919@qq.com'
+#EMAIL_HOST_USER =
 # 你的邮箱密码
-EMAIL_HOST_PASSWORD = 'hvtymfzuuawnbijf'
+#EMAIL_HOST_PASSWORD =
 # 发送邮件的端口
 EMAIL_PORT = 25
 # 是否使用 TLS
 EMAIL_USE_TLS = True
 # 默认的发件人
-DEFAULT_FROM_EMAIL = 'TJW的博客 <310533919@qq.com>'
+#DEFAULT_FROM_EMAIL =
 
 # 媒体文件地址
 MEDIA_URL = '/media/'
